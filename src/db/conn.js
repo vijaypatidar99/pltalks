c4582c11b32f77ebce272dd0bd47a094b28f097b
vijay patidar <87826203+vijaypatidar99@users.noreply.github.com>
2023-09-05 23:40:39 +0530

    Update conn.js
    update

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 
 
-mongoose.connect("mongodb+srv://vijay:%40vijay9926@cluster0.izbukiw.mongodb.net/placement",{
+mongoose.connect("#",{
     useNewUrlParser:true,useUnifiedTopology:true
 }).then(()=>{
     console.log("connection successfull");
